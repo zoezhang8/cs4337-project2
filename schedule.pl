a274b08af204304db2f75b9382ccf240abf20622
zoezhang8 <zoeqzhang8@gmail.com>
2026-05-04 23:32:17 -0500

    Add schedule.pl with plan/1

---
+++ schedule.pl
@@ -0,0 +1,17 @@
+%CS 4347 project 2- work schedule planner
+% Zoe Zhang
+% May 2026
+
+% This file implements plan/1 for scheduling 
+employees across shifts and workstations subject 
+to constraints. 
+
+% plan - main predicate that generates a valid 
+schedule
+
+plan(plan(Morning, Evening, Night)) :-
+% get all employees
+% get all workstations w their requirements
+% assign empllyees to shifts and workstations
+% verify all constraints
+fail.
