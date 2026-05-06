7ec1458e9e9387f08ba1cebdbd2d306bcedcb4a4
zoezhang8 <zoeqzhang8@gmail.com>
2026-05-06 13:16:47 -0500

    updated test input 1 using the example input given for the assignment

---
+++ test-input1.pl
@@ -0,0 +1,37 @@
+employee(ulysses).
+employee(bob).
+employee(zoey).
+employee(ximena).
+employee(jack).
+employee(harry).
+employee(mira).
+employee(erica).
+employee(tim).
+employee(peter).
+employee(larry).
+employee(ginny).
+employee(daniel).
+employee(yusuf).
+employee(ophelia).
+employee(frank).
+employee(walter).
+employee(charlie).
+employee(venessa).
+employee(sarah).
+employee(ryan).
+employee(quinn).
+employee(alice).
+employee(kimberly).
+employee(nick).
+employee(iris).
+
+workstation(1,2,4).
+workstation(2,5,9).
+workstation(3,1,1).
+
+workstation_idle(3,morning).
+
+avoid_workstation(ophelia, 3).
+avoid_workstation(ophelia, 1).
+
+avoid_shift(daniel, night).
