87ccebd7acc3dd46697e8af742108f397452a737
zoezhang8 <zoeqzhang8@gmail.com>
2026-05-06 13:15:28 -0500

    working shift assignment with avoid shift constraints, tested with test-shift-only

--- schedule.pl
+++ schedule.pl
@@ -1,8 +1,30 @@
 get_all_employees(Employees) :-
     findall(E, employee(E), Employees).
 
+assign_shifts([], [], [], []).
+
+assign_shifts([E|Rest], [E|MorningRest], Evening, 
+Night) :-
+    \+ avoid_shift(E, morning),
+    assign_shifts(Rest, MorningRest, Evening, 
+Night).
+
+assign_shifts([E|Rest], Morning, [E|EveningRest], 
+Night) :-
+    \+ avoid_shift(E, evening),
+    assign_shifts(Rest, Morning, EveningRest, 
+Night).
+
+assign_shifts([E|Rest], Morning, Evening, 
+[E|NightRest]) :-
+    \+ avoid_shift(E, night),
+    assign_shifts(Rest, Morning, Evening, 
+NightRest).
+
 plan(plan(Morning, Evening, Night)) :-
     get_all_employees(AllEmployees),
-    Morning = [workstation(1, AllEmployees)],
-    Evening = [],
-    Night = [].
+    assign_shifts(AllEmployees, MorningEmps, 
+EveningEmps, NightEmps),
+    Morning = [workstation(1, MorningEmps)],
+    Evening = [workstation(1, EveningEmps)],
+    Night = [workstation(1, NightEmps)].
